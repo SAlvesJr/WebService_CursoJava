3f4c3ce8b4632c84b205392ba82063fcc5e00a8f
Sergio Alves <sergio.alves@dce.ufpb.br>
2020-01-08 21:41:02 -0300

    Product

---
+++ src/main/java/com/SAlvesjr/webservice/resources/ProductResource.java
@@ -0,0 +1,33 @@
+package com.SAlvesjr.webservice.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.SAlvesjr.webservice.entities.Product;
+import com.SAlvesjr.webservice.service.ProductService;
+
+@RestController
+@RequestMapping(value = "/products")
+public class ProductResource {
+
+	@Autowired
+	private ProductService service;
+
+	@GetMapping
+	public ResponseEntity<List<Product>> findAll() {
+		List<Product> list = service.findAll();
+		return ResponseEntity.ok(list);
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Product> findById(@PathVariable Long id) {
+		Product obj = service.findById(id);
+		return ResponseEntity.ok(obj);
+	}
+}
---
+++ src/main/java/com/SAlvesjr/webservice/service/ProductService.java
@@ -0,0 +1,27 @@
+package com.SAlvesjr.webservice.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.SAlvesjr.webservice.entities.Product;
+import com.SAlvesjr.webservice.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+
+	@Autowired
+	private ProductRepository userRository;
+
+	public List<Product> findAll() {
+		return userRository.findAll();
+	}
+
+	public Product findById(Long id) {
+		Optional<Product> obj = userRository.findById(id);
+		return obj.get();
+	}
+
+}
